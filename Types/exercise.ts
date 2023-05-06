6b3a956120167f24dd0218b4fafd444792f834a5
pernillakack <112933591+pernillakack@users.noreply.github.com>
2023-05-06 20:04:20 +0200

    first commit

---
+++ Types/exercise.ts
@@ -0,0 +1,7 @@
+export type Exercise = {   
+   musclegroup: string
+   sets: number
+   reps: number
+   exercise: string
+   name: string
+}
---
+++ Types/workout.ts
@@ -0,0 +1,9 @@
+export type Workout = {
+    _id?: string    
+   musclegroup: string
+   sets: number
+   reps: number
+   exercise: string
+   weight?: number
+   name: string   
+}
